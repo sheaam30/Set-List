ce6d567c93c24408c47881563298e54e47715645
Adam <adam@Melissas-MacBook-Air.local>
2017-12-30 13:12:35 -0500

    - Add SetListActivity

---
+++ app/src/main/java/setlist/shea/setlist/setlists/SetListAdapter.kt
@@ -0,0 +1,34 @@
+package setlist.shea.setlist.setlists
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import setlist.shea.domain.model.SetList
+import setlist.shea.setlist.R
+
+/**
+ * Created by adam on 12/29/17.
+ */
+class SetListAdapter: RecyclerView.Adapter<SetListViewHolder>() {
+
+    var setLists: List<SetList> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): SetListViewHolder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_set_list, parent, false)
+        return SetListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SetListViewHolder?, position: Int) {
+        holder?.textView?.text = setLists[position].listName
+    }
+
+    override fun getItemCount(): Int = setLists.size
+}
+
+class SetListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    var textView: TextView = itemView.findViewById(R.id.textView)
+
+}
